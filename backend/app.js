60052aadf7eb84bccc33fc6afdc99e16e9b57807
aayush patidar <ap6887980@gmail.com>
2025-11-21 21:44:52 +0530

    i completed register and login route and data goes on online claud of mongo Atlas

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,18 @@ import { Server } from "socket.io";
 import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import { connnectToSocket } from "./src/controllers/socketManager.js";
+import userRoutes from "./src/routes/users.routes.js";
 
-const app = express();
-const server = createServer(app);
-const io = connnectToSocket(server);
+const app = express(); // it's normal express server
+const server = createServer(app); // convert express server to http-server
+const io = connnectToSocket(server); // http server attache to socket.io
 
 app.set("port", process.env.PORT || 8000);
 app.use(cors());
-app.use(express.json({ limit: "40kb" }));
+app.use(express.json({ limit: "40kb" })); // accepts data under the 40*1000 letters,& it's Enough
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 app.get("/home", (req, res) => {
   return res.json({ hello: "world" });
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,58 @@
+import { User } from "../models/user.model.js";
+import httpStatus from "http-status";
+import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please provide all credentials" });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User not found" });
+    }
+    // bcrypt.compare():this method internally changed plain text to hashed then compare
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+      user.token = token;
+      await user.save();
+      return res.status(httpStatus.OK).json({ token: token }); // token send to frontend
+    }
+  } catch (error) {
+    return res.status(500).json({ message: `Something went wrong${error}` });
+  }
+};
+
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      // it's called early return statement
+      return res
+        .status(httpStatus.FOUND)
+        .json({ message: "User exists already" });
+    }
+
+    // password ko 10 baar encrypt kiya ja raha hai for strong security
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    return res.status(httpStatus.CREATED).json({ message: "User registered" });
+  } catch (error) {
+    return res.json(`Something went wrong ${error}`);
+  }
+};
+
+export { register, login };
